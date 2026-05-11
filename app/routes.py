be7a4a9d14fa60c640a7c4e33b5178ae728a2f32
Flare-Blitz <jacksonkeeler@gmail.com>
2026-05-11 17:37:09 -0500

    feat: Index page now displays all users, and redirects to a user page on click.

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template
 
-from .models import Trainer
+from app.models import (
+    PokemonSpecies, AbilityList, Pokemon_Ability, MoveList, Pokemon_Move,
+    Trainer, HeldItem, Trainer_HeldItem, PokemonInstance, Team
+)
 
 main = Blueprint("main", __name__)
 
 
 @main.route("/")
 def index():
-    records = Trainer.query.order_by(Trainer.id.desc()).all()
-    return render_template("index.html", records=records)
+    trainers = Trainer.query.order_by(Trainer.id.desc()).all()
+    return render_template("index.html", trainers=trainers)
